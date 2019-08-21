e746b83454779a030692c90b1657fd256abaa8df
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-08-21 19:29:09 -0300

    Add user model;

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'humbreto',
+    email: '@',
+    password_hash: '1823721786312',
+  });
+
+  return res.json(user);
+});
 export default routes;
