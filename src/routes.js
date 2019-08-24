ef9a631d014177c213a3b6b90d1a742f9127e1b3
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-08-24 20:07:03 -0300

    Add ProviderController.

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
-
+import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -15,6 +15,13 @@ routes.post('/users', UserController.store);
 routes.put('/users', authMiddleware, UserController.update);
 routes.post('/sessions', SessionController.store);
 
-routes.post('/files', upload.single('file'), FileController.store);
+routes.post(
+  '/files',
+  upload.single('file'),
+  authMiddleware,
+  FileController.store
+);
+
+routes.get('/providers', authMiddleware, ProviderController.index);
 
 export default routes;
