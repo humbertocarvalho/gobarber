36fea3fe1db2c3361163048bf38b02f4e8050d6c
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-08-24 23:53:22 -0300

    Create store method to appointments controller.

--- src/routes.js
+++ src/routes.js
@@ -7,14 +7,19 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
+// Rotas de Usuário
 routes.post('/users', UserController.store);
 routes.put('/users', authMiddleware, UserController.update);
+
+// Rotas de Sessão
 routes.post('/sessions', SessionController.store);
 
+// Rotas de Arquivo
 routes.post(
   '/files',
   upload.single('file'),
@@ -22,6 +27,10 @@ routes.post(
   FileController.store
 );
 
+// Rotas de Providers
 routes.get('/providers', authMiddleware, ProviderController.index);
+
+// Rotas de Agendamentos
+routes.post('/appointments', authMiddleware, AppointmentController.store);
 
 export default routes;
