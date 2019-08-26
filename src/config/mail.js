c10ded4d1eeefd3fcb4145c7555be83f219aa316
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-08-26 19:51:27 -0300

    Add email feature.

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'e8c3e8be82baf6',
+    pass: '9de9e5b40c53d0',
+  },
+  secure: false,
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
