3b340aeb61f5bd2ce11234ed733e9a96188f08d9
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-08-27 20:36:31 -0300

    Add .env variables.

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,9 +1,9 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   auth: {
-    user: 'e8c3e8be82baf6',
-    pass: '9de9e5b40c53d0',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   secure: false,
   default: {
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://6c5d7c01ab584409af524d391f8f7e71@sentry.io/1543656',
+  dsn: process.env.SENTRY_DSN,
 };
