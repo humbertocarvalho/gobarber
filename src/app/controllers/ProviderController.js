3211470ab2fc12ab445f5da1e34900f537359a07
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-09-24 21:10:13 -0300

    Created providers cache.

--- src/app/controllers/ProviderController.js
+++ src/app/controllers/ProviderController.js
@@ -1,8 +1,16 @@
 import User from '../models/User';
 import File from '../models/File';
 
+import Cache from '../../lib/Cache';
+
 class ProviderController {
   async index(req, res) {
+    const cachedProviders = await Cache.get('providers');
+
+    if (cachedProviders) {
+      return res.json(cachedProviders);
+    }
+
     const providers = await User.findAll({
       where: { provider: true },
       attributes: ['id', 'name', 'email', 'avatar_id'],
@@ -10,6 +18,9 @@ class ProviderController {
         { model: File, as: 'avatar', attributes: ['name', 'path', 'url'] },
       ],
     });
+
+    await Cache.set('providers', providers);
+
     return res.json(providers);
   }
 }
