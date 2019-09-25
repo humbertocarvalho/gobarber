394b765f3a36945c96b881fb66d79300e7da80cb
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-09-25 19:49:02 -0300

    Add express-brute-force.

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import Brute from 'express-brute';
+import BruteRedis from 'express-brute-redis';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
@@ -21,12 +23,24 @@ import validateAppointmentStore from './app/validators/AppointmentStore';
 const routes = new Router();
 const upload = multer(multerConfig);
 
+const bruteStore = new BruteRedis({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIST_PORT,
+});
+
+const bruteForce = new Brute(bruteStore);
+
 // Rotas de Usuário
 routes.post('/users', validateUserStore, UserController.store);
 routes.put('/users', authMiddleware, validateUserUpdate, UserController.update);
 
 // Rotas de Sessão
-routes.post('/sessions', validateSessionStore, SessionController.store);
+routes.post(
+  '/sessions',
+  bruteForce.prevent,
+  validateSessionStore,
+  SessionController.store
+);
 
 // Rotas de Arquivo
 routes.post(
